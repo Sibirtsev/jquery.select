72f7b5c39e9ad4c0fab9f5aa9aa5cfc3ea37dae4
Alexey Sibirtsev <a.sibirtsev@office.ngs.ru>
2013-03-27 13:17:06 +0700

    second

--- jquery.select.js
+++ jquery.select.js
@@ -1,47 +1,74 @@
-(function($) {
-  $.fn.select = function() {
-    return this.each(function() {
-      var $this = $(this);
-      var $selector = $this;
-      var $select = $selector.find('select');
-      var $options = $select.find('option');
+;(function($, window, document, undefined) {
+    var pluginName = 'select',
+        defaults = {
+            selected: 0
+        };
 
-      var $optionsDataValues = [];
-      var $optionsDataTexts = [];
+    function Plugin( element, options ) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
 
-      $options.each(function(key, value){
-        $optionsDataTexts.push($(value).text());
-        $optionsDataValues.push($(value).val());
-      });
+        this.init();
+    }
+    Plugin.prototype = {
 
-      $select.hide();
+        init: function() {
+            var _this = this;
+            var $container = $(this.element);
+            var $selector = $container;
+            var $select = $selector.find('select');
+            var $options = $select.find('option');
 
-      var $input = $("<input type='text' name='" + $select.attr('name') + "_input' value='' readonly autocomlete='off' />");
-      var $list = $("<ul class="list" style="display: none;">");
-      for(var i in $optionsDataTexts) {
-        $list.append($("<li>" + $optionsDataTexts[i] + "</li>"));
-      }
+            var $input = $('<span class="select-text" />');
 
-      $list.find('li').click(function(e){
-        var selected = $(this).text();
-        var index = $.inArray(selected, $optionsDataTexts);
-        $input.val(selected);
-        $select.val($optionsDataValues[index]);
-        $list.hide();
-      });
+            var $optionsDataValues = {};
 
-      $input.click(function(){
-        $list.show()
-      });
-      $list.mouseover(function(){
-        $list.show();
-      });
-      $list.mouseout(function(){
-        $list.hide();
-      });
+            $options.each(function(key, value) {
+                var optionValue = $(value).val();
+                var optionText = $(value).text();
+                $optionsDataValues[optionValue] = optionText;
+                if (optionValue == _this.options.selected) {
+                    $input.text(optionText);
+                }
+            });
 
-      $selector.append($input);
-      $selector.append($list);
-    });
-  }
-})(jQuery)
+            $select.hide();
+
+            var $list = $('<ul class="list" style="display: none;">');
+            for (var i in $optionsDataValues) {
+                $list.append($('<li data-value="' + i + '">' + $optionsDataValues[i] + '</li>'));
+            }
+
+            $list.find('li').click(function(e) {
+                var selected = $(this).text();
+                var index = $(this).attr('data-value');
+                $input.text(selected);
+                $select.val(index);
+                $list.hide();
+            });
+
+            $input.click(function(){
+                $list.show()
+            });
+            $list.mouseover(function(){
+                $list.show();
+            });
+            $list.mouseout(function(){
+                $list.hide();
+            });
+
+            $container.append($input);
+            $container.append($list);
+        }
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+            }
+        });
+    };
+})(jQuery, window, document);
